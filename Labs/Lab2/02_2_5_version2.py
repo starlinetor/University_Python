a32a182a6eb2c57394f60c7b0d181ec8b26898c0
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-10-11 20:15:47 +0200

    Added version 2 for a lab exercise

---
+++ Labs/Lab2/02_2_5_version2.py
@@ -0,0 +1,21 @@
+def controlla_matricola(matricola: str) -> None:
+    if not(matricola[0:1] is str and matricola[1:].isnumeric()):
+        raise ValueError('Il valore "{}" è sbagliato, necessario come primo carattere una lettera seguito da un numero\nexp : s343243'.format(matricola))
+
+#imput
+matricola_1: str = input("Inserisci il primo numero di matricola:\n")
+matricola_2: str = input("Inserisci il secondo numero di matricola:\n")
+
+#verifica corettezza imput
+controlla_matricola(matricola_1)
+controlla_matricola(matricola_2)
+
+#estrazione numero di matricola
+n_matricola_1: int = int(matricola_1[1:])
+n_matricola_2: int = int(matricola_2[1:])
+
+#print a schermo
+if n_matricola_1 > n_matricola_2 :
+    print(matricola_1,"\t",matricola_2)
+else :
+    print(matricola_2,"\t",matricola_1)
