96ebbf7434928a75d42997bcc7c951c8b98732d0
Edoardo Arturo Cancelli <67192582+starlinetor@users.noreply.github.com>
2024-10-17 17:31:39 +0200

    Update 03.1.5.py
    fixed missing f string

--- Labs/Lab3/03.1.5.py
+++ Labs/Lab3/03.1.5.py
@@ -5,7 +5,7 @@ disequation_2: bool = x > 0 or x < 100
 disequation_3: bool = x > 0 or 100 < x
 disequation_4: bool = x > 0 and x < 100 or x == -1
 
-print("{x} > 0 and {x} < 100", disequation_1)
-print("{x} > 0 or {x} < 100", disequation_2)
-print("{x} > 0 or 100 < {x}", disequation_3)
-print("{x} > 0 and{x} < 100 or {x} == -1", disequation_4)
+print(f"{x} > 0 and {x} < 100", disequation_1)
+print(f"{x} > 0 or {x} < 100", disequation_2)
+print(f"{x} > 0 or 100 < {x}", disequation_3)
+print(f"{x} > 0 and{x} < 100 or {x} == -1", disequation_4)
