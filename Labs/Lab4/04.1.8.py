cec4d8a5ac55d241677f794d4d3d822dc3f6bd78
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-10-21 19:26:11 +0200

    Done 4.1.8

--- Labs/Lab4/04.1.8.py
+++ Labs/Lab4/04.1.8.py
@@ -1,10 +1,25 @@
+sequence: bool = False
+sequence_number: int = 0
+sequence_number_init: bool = False
+valid_numbers: list[int] = []
+
 while True:
 
-    value:str = input("Enter a new number: ")
+    value_str:str = input("Enter a new number: ")
 
-    if(value == ""):
-        exit()
+    if(value_str == ""):
+        break
 
-    numbers.append(int(value))
+    value: int = int(value_str)
 
-    
+    if(sequence_number_init == False):
+        sequence_number_init = True
+        sequence_number = -value
+
+    if (value == sequence_number and value not in valid_numbers):
+        valid_numbers.append(value) 
+    else:
+        sequence_number = value
+
+print(valid_numbers)
+
