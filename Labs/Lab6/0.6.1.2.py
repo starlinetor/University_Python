02db0eb7a101865e29213aa92335bfebf169eb91
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-04 11:02:11 +0100

    Done 6.1.2

---
+++ Labs/Lab6/0.6.1.2.py
@@ -0,0 +1,17 @@
+def count_words (string : str) -> int:
+    """
+    Returns the number of words in a given string\n
+    A word is a sequence of character separated by 1 or more spaces
+    """
+    
+    word : bool = False
+    words : int = 0
+    
+    for char in string:
+        if not word and char != " ":
+            word = True
+            words +=1
+        if word and char == " ":
+            word = False
+    
+    return words
