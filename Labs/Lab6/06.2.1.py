abcaaf60c9eecdc363a349b048e0d28095732cfe
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-04 20:55:49 +0100

    Started 6.2.1

---
+++ Labs/Lab6/06.2.1.py
@@ -0,0 +1,10 @@
+def subsidy_calculator(income: float, kids: int):
+    
+
+def main():
+    while True:
+        if input("Press enter to continue or -1 to exit") == "-1": 
+            break
+
+if __name__ == "__main__":
+    main()
