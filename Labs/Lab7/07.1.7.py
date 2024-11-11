7f281809d6301606d0cc71a67fe0dc843f04ad36
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-11 10:01:55 +0100

    Done for real 07.1.7

--- Labs/Lab7/07.1.7.py
+++ Labs/Lab7/07.1.7.py
@@ -2,12 +2,15 @@ import random
 
 random_list : list[int] = []
 
-for i in range(20):
+for i in range(3):
     random_list.append(random.randint(0,99))
     
-def sum_without_lowestl(numbers: list[int]):
+def sum_without_lowest(numbers: list[int]):
     numbers.remove(min(numbers))
     sum_n : int = 0
     for n in numbers:
         sum_n += n
     return sum_n
+
+print(random_list)
+print(sum_without_lowest(random_list))
