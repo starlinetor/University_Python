d141942f9ee64042179051093b158e324ce901ba
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-18 10:33:05 +0100

    Done 8.1.5

---
+++ Labs/Lab8/08.1.5.py
@@ -0,0 +1,23 @@
+def merge(list_a : list, list_b : list) -> list:
+    if len(list_a) < len(list_b) : 
+        short_lenght : int = len(list_a)
+        long_list : bool = list_b
+    else : 
+        short_lenght : int = len(list_b)
+        long_list : list = list_a
+        
+    merged_list : list = []
+    
+    for i in range(short_lenght):
+        merged_list.append(list_a[i])
+        merged_list.append(list_b[i])
+        
+    for i in range(short_lenght, len(long_list),1):
+        merged_list.append(long_list[i])
+    return merged_list
+
+a = ["a","b","c","d"]
+b = [1,2,3,4,5,6,7,8,9]
+
+print(merge(a,b))
+print(merge(b,a))
