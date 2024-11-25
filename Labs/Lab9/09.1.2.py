6e99fea790a478b157a2a7f0f4ea8d78d44f83ab
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-25 09:28:42 +0100

    Done 09.1.2

---
+++ Labs/Lab9/09.1.2.py
@@ -0,0 +1,17 @@
+list_1 = [i for i in range(1,11)]
+list_2 = [i * 2 for i in range(11)]
+list_3 = [i*i for i in range(1,11)]
+list_4 = [0 for _ in range(11)]
+#no clue of the pattern
+list_5 = [1, 4, 9, 16, 9, 7, 4, 9, 11]
+list_6 = [i%2 for i in range(10)]
+list_7 = [i%5 for i in range(10)]
+
+
+print(list_1)
+print(list_2)
+print(list_3)
+print(list_4)
+print(list_5)
+print(list_6)
+print(list_7)
