d6af4692e4aa3b7f23efafbf6526ca6570a7e764
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2024-11-25 09:34:21 +0100

    Done 09.1.3

--- Labs/Lab9/09.1.2.py
+++ Labs/Lab9/09.1.2.py
@@ -2,7 +2,7 @@ list_1 = [i for i in range(1,11)]
 list_2 = [i * 2 for i in range(11)]
 list_3 = [i*i for i in range(1,11)]
 list_4 = [0 for _ in range(11)]
-#no clue of the pattern
+#no clue on what the pattern is
 list_5 = [1, 4, 9, 16, 9, 7, 4, 9, 11]
 list_6 = [i%2 for i in range(10)]
 list_7 = [i%5 for i in range(10)]
---
+++ Labs/Lab9/09.1.3.py
@@ -0,0 +1,15 @@
+import random
+
+
+def bar_code(values : list) -> None:
+    max_value = max(values)
+    asteriscs_for_unit = 40/max_value
+    
+    for value in values:
+        print("*" * int(asteriscs_for_unit * value))
+
+values = [random.randint(1,40) for _ in range(10)]
+
+print(values)
+
+bar_code(values)
